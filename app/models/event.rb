34899ebfab8c628c249f8d0f822ead74463b821d
Miguel Tucu Gomez <miguelgomez66@gmail.com>
2021-03-25 13:51:54 -0300

    Update associations using through relation

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,9 @@
 class Event < ApplicationRecord
+  has_many :registrations
+  has_many :attendees, through: :registrations, source: :user
+
+
   belongs_to :creator, class_name: 'User', foreign_key: "user_id"
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
+  has_many :registrations
+  has_many :attended_events, through: :registrations, source: :event
+
+
   has_many :events, inverse_of: :creator
+  validates :name, uniqueness:  true
+
 end
