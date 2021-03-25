1ed13c85e3055ba4099675da2addae2966ca5b68
George <71434803+george-swift@users.noreply.github.com>
2021-03-25 15:50:26 +0100

    Add events index and show pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
     end
   end
 
-  def show∫
+  def show
     @user = User.find(params[:id])
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,5 @@
 Rails.application.routes.draw do
-  # get 'events/index'
-  # get 'events/new'
-  # get 'events/show'
   root 'events#index'
-  # get 'users/new'
-  # post 'users/new', to: 'users#create'
-  # get 'users/show'
-  # get 'sessions/new'
-  # post 'sessions/new', to: 'sessions#create'
 
   get '/signup', to: 'users#new'
   get '/login', to: 'sessions#new'
@@ -16,6 +8,5 @@ Rails.application.routes.draw do
 
   resources :users
   resources :events
-  # resources :sessions
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
