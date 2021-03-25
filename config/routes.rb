3c53e5c92815df9aba3b3517c1a60b5a94c5ab50
George <71434803+george-swift@users.noreply.github.com>
2021-03-25 15:49:26 +0100

    Refactor routes and change links

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,21 @@
 Rails.application.routes.draw do
-  get 'events/index'
-  get 'events/new'
-  get 'events/show'
+  # get 'events/index'
+  # get 'events/new'
+  # get 'events/show'
   root 'events#index'
-  get 'users/new'
-  post 'users/new', to: 'users#create'
-  get 'users/show'
-  get 'sessions/new'
-  post 'sessions/new', to: 'sessions#create'
+  # get 'users/new'
+  # post 'users/new', to: 'users#create'
+  # get 'users/show'
+  # get 'sessions/new'
+  # post 'sessions/new', to: 'sessions#create'
 
+  get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
   resources :users
-  resources :sessions
+  resources :events
+  # resources :sessions
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
