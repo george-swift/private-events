a0e0480fb7f5eb3ae32243124a3817300994d33a
Miguel Tucu Gomez <miguelgomez66@gmail.com>
2021-03-26 12:22:28 -0300

    Make registration create method accept an array with attendees id's

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,15 +1,27 @@
 class RegistrationsController < ApplicationController
 
   def create
-    # event = Event.find(params[:event_id])
-    puts params
-    registered = Registration.new(event_id: params[:event_id], user_id: current_user.id)
-    if registered.save
-      flash[:success] = 'Invitation sent!'
-      redirect_to users_path(event_id: event_id)
+
+    event = Event.find(params[:event_id])
+    users_invited = params[:invited_users]
+    errors = []
+
+    users_invited.each do |attendee|
+
+      registered = Registration.new(event_id: event.id, user_id: attendee)
+
+      if registered.save
+      else
+        errors << attendee
+      end
+      end
+
+    if errors.size > 0
+      flash.now[:danger] = 'This erorr will never pop up'
     else
-      flash.now[:danger] = 'Something is not right'
-      redirect_to event_path(event)
+      flash.now[:success] = 'All users have been invited'
     end
+
+    redirect_to event_path(event)
   end
 end
