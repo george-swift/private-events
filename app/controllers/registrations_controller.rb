ccaede102df39add8f80f325a2e6953287e7715c
George <71434803+george-swift@users.noreply.github.com>
2021-03-27 15:45:58 +0100

    Refactor code in controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,22 +1,20 @@
 class RegistrationsController < ApplicationController
 
   def create
-
     event = Event.find(params[:event_id])
     users_invited = params[:guests_list]
-    errors = []
 
-    users_invited.each do |attendee|
-      registered = Registration.new(event_id: event.id, user_id: attendee)
-      errors << attendee unless registered.save
-    end
-
-    if errors.size.positive?
-      flash.now[:danger] = 'This erorr will never pop up'
+    if users_invited.all?(&:blank?)
+      flash[:warning] = 'Invitations will be available with more users'
+      redirect_to event_path(event)
     else
-      flash.now[:success] = 'All users have been invited'
-    end
+      users_invited.each do |attendee|
+        registered = Registration.new(event_id: event.id, user_id: attendee)
+        registered.save
+      end
 
-    redirect_to event_path(event)
+      flash[:success] = 'Selected guest(s) successfully added to attendees!'
+      redirect_back(fallback_location: event_path(event))
+    end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,13 +10,14 @@ class UsersController < ApplicationController
       flash[:success] = 'Welcome to Private Events'
       redirect_to @user
     else
-      render :new
       flash[:danger] = 'Invalid signup details'
+      render :new
     end
   end
 
   def show
     @user = User.find(params[:id])
+    @info = @user.events.ids.to_enum
   end
 
   private
