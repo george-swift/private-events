7ae4b7cd3276df5aa56b82c11f8bd593a849d3a7
Miguel Tucu Gomez <miguelgomez66@gmail.com>
2021-03-30 11:53:29 -0300

    Update scope in event controller to cover edge case and correct first error (using minimime instead of miemagic gem)

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,6 +3,6 @@ class Event < ApplicationRecord
   has_many :attendees, through: :registrations, source: :user
 
   belongs_to :creator, class_name: 'User', foreign_key: 'user_id'
-  scope :upcoming, -> { where('date > ?', Time.now) }
+  scope :upcoming, -> { where('date >= ?', Time.now) }
   scope :previous, -> { where('date < ?', Time.now) }
 end
